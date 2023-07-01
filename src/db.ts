d08063d1e235cd0c353cb50fae7c5604bd0bd5d7
erekle ratiani <62502052+eratiani@users.noreply.github.com>
2023-07-02 02:08:31 +0400

    fix issue with .ts extention error

--- src/db.ts
+++ src/db.ts
@@ -1,6 +1,6 @@
 // const { v4: uuidv4 } = require("uuid");
 import { v4 as uuidv4 } from "uuid";
-import { user } from "./shared/interfaces/user";
+import { user } from "./shared/interfaces/user.js";
 const users: user[] = [];
 
 function getAllUsers() {
